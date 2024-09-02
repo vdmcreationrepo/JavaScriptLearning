39cff1ab3f181b103d9433fd8b5de89ecb57a54b
Videsh Makwana <167800094+vdmcreationrepo@users.noreply.github.com>
2024-09-02 17:54:33 +0000

    learn datatypes

---
+++ sep-2/datatypes.js
@@ -0,0 +1,17 @@
+let name ='Videsh' // String
+let age=27 // Number
+let isLoggedIn=false // Boolean
+
+// primitive data types
+// number ->range 2 to power 53
+// string => ""
+// boolean => true/false (yes or no)
+// null => special type,standalone value
+// undefined => value not assigned
+//symbol => used for uniqueness
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
