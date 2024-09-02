b8798713a05ebf5e3fd645e95464b691ccc0afe3
Videsh Makwana <167800094+vdmcreationrepo@users.noreply.github.com>
2024-09-02 17:30:10 +0000

    learn variables (var, let and const)

---
+++ sep-2/variables.js
@@ -0,0 +1,28 @@
+const firstName='FirstName';
+let lastName='LastName';
+var password='Hello@234';
+var password='Hello@234';
+confirmPassword="Hello@234"; // without using var,let or const we can create variables
+
+
+// firstName='Abcd';
+//console.log(firstName); not allowed to change const value
+// const used to define constant variable.  next time no one can change
+// if we use var for the variable declaration then var allow us to create variable with same name.
+// bellow example (prefer not to use var for the variable declaration)
+// var has scope issue,
+var myName="MyName";
+var myName="Hello"
+
+lastName='Abcd';
+password='Passss';
+confirmPassword='confirmPassword';
+
+// console.log(lastName);
+// console.log(password);
+// console.log(confirmPassword);
+
+
+
+var firstValue;
+console.log(firstValue); // if we not give any value then js consider as undefined
