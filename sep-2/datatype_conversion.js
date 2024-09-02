bad7a15589bc69e8efce5e2cfd7457bc92d728fc
Videsh Makwana <167800094+vdmcreationrepo@users.noreply.github.com>
2024-09-02 18:14:09 +0000

    learn datatype conversion

---
+++ sep-2/datatype_conversion.js
@@ -0,0 +1,21 @@
+// let score="33"
+// let score=null
+// let score=undefined
+let score=false
+console.log(typeof score); // check variable type
+
+// let value=Number(score);
+// console.log(typeof value);
+// console.log(value);
+
+// try to convert number -> easily converted
+// "22" -> converted value NAN
+//  false-> converted to 1
+
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "Videsh" => true
