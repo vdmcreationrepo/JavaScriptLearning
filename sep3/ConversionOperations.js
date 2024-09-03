544621ccbac3353356e8aa4465b89c7373b80184
Videsh Makwana <167800094+vdmcreationrepo@users.noreply.github.com>
2024-09-03 03:38:48 +0000

    Learn conversion operations

---
+++ sep3/ConversionOperations.js
@@ -0,0 +1,40 @@
+// console.log("hello");
+
+// --------------------Operations------------------
+// let value=3;
+// let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1='Videsh'
+let str2=' Makwana'
+
+let str3=str1+str2;
+// console.log(str3);
+
+// complex situations.
+
+// console.log(1+'2');
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let value,value1,value2
+value=value1=value2=2+2
+
+let gameCounter=100
+gameCounter++
+++gameCounter
+console.log(gameCounter);
