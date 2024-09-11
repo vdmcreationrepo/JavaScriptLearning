a07af763513643066f37c7cd6300c7b3c6150d2c
Videsh Makwana <167800094+vdmcreationrepo@users.noreply.github.com>
2024-09-11 04:28:38 +0000

    Learn Array Of Joining

---
+++ sep-11/joining.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.join(","); // this will return string with coma
+// result : Banana,Orange,Apple,Mango String Type
+
+console.log(text);
