5eb1565adc5f5e6cee1a9297c75c459e41e20a68
Videsh Makwana <167800094+vdmcreationrepo@users.noreply.github.com>
2024-09-18 05:42:33 +0000

    Learn Math And Number related functions

---
+++ sep-14/strings.js
@@ -0,0 +1,6 @@
+const name ='Videsh'
+const repoCount=100;
+
+// console.log(name +repoCount);
+const secondString="hello ${repoCount}"
+console.log(secondString);
---
+++ sep-16/NumberAndMaths.js
@@ -0,0 +1,12 @@
+const number=1000;
+// automatically defined as number
+// console.log(number);
+
+
+const number1 =new Number(100);
+// define number explicitly
+// console.log(number1);
+
+const no=1000000000;
+// console.log(no.toLocaleString());// Number print on us time zone
+// console.log(no.toLocaleString('en-IN')); // Number format will be on India number
